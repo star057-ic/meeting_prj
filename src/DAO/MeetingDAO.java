0b74e691ba07565b50022e20c27548044c153dcf
seoyk <seobear95@gmail.com>
2025-12-01 11:45:42 +0900

    MeetingDAO 수정, 삭제 추가 완료

--- src/DAO/MeetingDAO.java
+++ src/DAO/MeetingDAO.java
@@ -21,8 +21,49 @@ public class MeetingDAO {
     }
 
     // 삭제
+    public static void deleteMeeting(Connection con, int meeting_id){
+        String sql = "DELETE FROM meetings WHERE id = ?";
+        try{
+            PreparedStatement pst = con.prepareStatement(sql);
+            pst.setInt(1, meeting_id);
+            int rows = pst.executeUpdate();  // 1 or 0
+            if(rows > 0){
+                System.out.println("회의 정보 삭제 성공: " +  meeting_id+ " 삭제 완료");
+            }else{
+                System.out.println("회의 정보 삭제 불가: "+ meeting_id + " 정보 없음.");
+            }
+
+        } catch (SQLException e) {
+            System.out.println("회의 정보 삭제 중에 SQL예외 발생: "+ e.getMessage());
+        }
+    }
 
     // 수정
+    public static void updateMeeting(Connection con, String title, String date,
+                                      String location, int meeting_id)
+    {
+        String sql = "UPDATE meetings SET title = ?, date = ?, location = ? " +
+                     "WHERE id = ?";
+
+        try{
+            PreparedStatement pst = con.prepareStatement(sql);
+            pst.setString(1, title);
+            pst.setDate(2, Date.valueOf(date));
+            pst.setString(3, location);
+            pst.setInt(4, meeting_id);
+            int rows = pst.executeUpdate();
+            if(rows > 0){
+                System.out.println("회의 정보 수정 성공: " + meeting_id +" 수정 완료");
+            }else{
+                System.out.println("회의 정보 수정 실패: " + meeting_id +" 정보 없음");
+            }
+
+        }catch(Exception e){
+
+            System.out.println("회의 정보 수정 예외 발생: " + e.getMessage());
+        }
+
+    }
 
 
     // 조회
--- src/MAIN/MainTest.java
+++ src/MAIN/MainTest.java
@@ -14,11 +14,16 @@ public class MainTest {
         } catch (SQLException e) {
             System.out.println("DB 연결 실패!: " + e.getMessage());
         }
-        System.out.println(" CRUD 작업 가능");
-        MeetingDAO.insertMeeting(conn, "중간 결과 보고 회의2 ", "2025-10-24", "혁신관 300");
-        MeetingDAO.insertMeeting(conn, "중간 결과 보고 회의3 ", "2025-12-24", "혁신관 306");
+        //System.out.println(" CRUD 작업 가능 - insert test");
+        //MeetingDAO.insertMeeting(conn, "캡스톤회의01 ", "2025-10-24", "혁신관 304");
+        //MeetingDAO.insertMeeting(conn, "최종회의", "2025-12-24", "혁신관 306");
 
 
+      //  System.out.println(" CRUD 작업 가능 - delete test");
+      //  MeetingDAO.deleteMeeting(conn,9);
+      //  MeetingDAO.deleteMeeting(conn,8);
+
+         MeetingDAO.updateMeeting(conn,"수정제목변경","2025-12-01","304", 1);
 
     }
 }
